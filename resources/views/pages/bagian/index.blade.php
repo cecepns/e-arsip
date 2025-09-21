99eaa24c1a2ae729a073a768add087e8865cde14
Muhamad Rifky <muhamad210501@gmail.com>
2025-09-21 16:15:11 +0700

    Remove deprecated CSS link from 'bagian' index view to streamline the layout and improve maintainability.

--- resources/views/pages/bagian/index.blade.php
+++ resources/views/pages/bagian/index.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.admin')
 
-@push('head')
-<link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
-@endpush
-
 @section('admin-content')
 <div class="page-header">
     @include('partials.breadcrumb', [
