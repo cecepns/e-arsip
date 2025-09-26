b1d86a2591ba8740c5ca95e61a32ddcf7fb29de7
Muhamad Rifky <muhamad210501@gmail.com>
2025-09-27 01:45:56 +0700

    update delete modal footer bagian

--- resources/views/pages/bagian/_delete_modal/_footer.blade.php
+++ resources/views/pages/bagian/_delete_modal/_footer.blade.php
@@ -1,6 +1,6 @@
+<button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="deleteBagianCancelBtn">Batal</button>
 <form id="deleteBagianForm" method="POST" style="display: inline;">
     <input type="hidden" name="_method" value="DELETE">
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
     <button type="submit" class="btn btn-danger" id="deleteBagianSubmitBtn">Ya, Hapus</button>
 </form>
-<button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="deleteBagianCancelBtn">Batal</button>
