c779275148f2f0269531559cbbbe3d3604b9baf1
Muhamad Rifky <muhamad210501@gmail.com>
2025-09-29 00:27:05 +0700

    Add browser tab title support in page title partial using @section directive

--- resources/views/partials/page-title.blade.php
+++ resources/views/partials/page-title.blade.php
@@ -9,6 +9,10 @@
         'subtitle' => 'Kelola surat masuk dan surat keluar dengan mudah.'
     ])
 --}}
+
+{{-- ANCHOR: Set browser tab title using @section directive --}}
+@section('title', $title . ' | E-Arsip')
+
 <div class="page-title">
     <div class="d-flex justify-content-between align-items-center">
         <div>
